2059f2de6c56c44b2917e91543c897d860920b9d
code3743 <jota.lopez@correounivalle.edu.co>
2022-08-17 20:03:12 -0500

    Detalles y Busqueda

--- controllers/sistema_opac.controller.js
+++ controllers/sistema_opac.controller.js
@@ -23,7 +23,7 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
     if(!evaluarUsuario) {
         return  res.status(400).json({
             error: 'El usuario no existe',
-            });
+        });
     }
       const fechaExpiracion = await page.evaluate(()=>{
           return document.querySelector('#user_expdate_text').innerText;
@@ -57,7 +57,7 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
               return prestamos
           }
           document.querySelector('#tab_Title3').click();
-          return []
+          return [];
       });
   
       const historialPrestamos = await page.evaluate(()=>{
@@ -77,7 +77,7 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
               }
               return historial;
           }
-         return []
+         return [];
       });
       await navegador.close();
 
@@ -111,19 +111,19 @@ const actualizarLibro = async (req = request, res = response)=>{
         await page.waitForTimeout(5000);
     
         await page.evaluate((index)=>{
-          const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr')
-          libros[index].querySelectorAll('td')[5].querySelector('span>img').click() ;
+          const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr');
+          libros[index].querySelectorAll('td')[5].querySelector('span>img').click();
           return libros[index].querySelectorAll('td')[5].innerText;
         }, libro);
         await page.waitForTimeout(2000);
         const actualizarLibro = await page.evaluate((index)=>{
-            const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr')
+            const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr');
             return libros[index].querySelectorAll('td')[5].innerText;
           }, libro);
         await page.close();
         res.json({
-          estado: actualizarLibro
-        })
+          estado: [actualizarLibro]
+        });
   
     } catch (error) {
             res.status(500).send(`Algo salió mal: ${error}`);
@@ -152,14 +152,14 @@ const actualizarTodo = async (req = request, res = response)=>{
             const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr');
             const estados = new Array(libros.length - 1);
             for(let i=0; i<libros.length - 1; i++){
-                estados[i] = libros[i + 1].querySelectorAll('td')[5].innerText
+                estados[i] = libros[i + 1].querySelectorAll('td')[5].innerText;
             }
-           return estados
+           return estados;
         });
         await page.close();
         res.json({
           estado: actualizarLosLibros
-        })
+        });
   
     } catch (error) {
         res.status(500).send(`Algo salió mal: ${error}`);
@@ -167,8 +167,116 @@ const actualizarTodo = async (req = request, res = response)=>{
 };
 
 
+const buscador = async(req = request, res = response)=>{
+    const { parametro } = req.query;
+    const noImagen = req.protocol + '://' + req.get('host') + '/images/no-imagen.jpg';
+    try {
+        const navegador = await chromium.launch({ chromiumSandbox: false });
+        const page = await navegador.newPage();
+        await page.goto("https://opac.univalle.edu.co/cgi-olib/");
+        await page.waitForLoadState();
+        await page.fill('input#searchInputField1', parametro.replace('%20',' '));
+        await page.click('.searchButton');
+        await page.waitForTimeout(5000);
+        const resultados = await page.evaluate((url)=>{
+            if(document.querySelector('.title_hitlist>table>tbody') != null){
+                const resultados = document.querySelector('.title_hitlist>table>tbody').querySelectorAll('.hitlist_ticol');
+                const libros = new Array(resultados.length);
+                resultados.forEach((libro, index) =>{
+                    const id = libro.querySelector('.resultsbright>a').id.replace('hitlabel','');
+                    const nombre = libro.querySelector('.resultsbright').innerText;
+                    const autor = libro.querySelector('.extras').innerText.split('\n')[1];
+
+                    let ISBN = '';
+                    let detalle = '';
+                    let ejemplares = '';
+                    let imagen = '';
+                    
+                    if(libro.querySelector('.extras').innerText.includes('ISBN: ')){
+                        ISBN  = libro.querySelector('.extras').innerText.split('\n')[2].replaceAll('ISBN: ','');
+                        detalle  = libro.querySelector('.tihitlist_l3').innerText.split('\n')[0].replace(' ','');
+                        ejemplares = libro.querySelector('.tihitlist_l3').innerText.split('\n')[1];
+                        imagen = `http://covers.openlibrary.org/b/isbn/${ISBN}-L.jpg`
+                    }else{
+                        ISBN = '';
+                        detalle = libro.querySelector('.tihitlist_l3').innerText.split('\n')[0].replace(' ','');
+                        ejemplares = libro.querySelector('.tihitlist_l3').innerText.split('\n')[1];
+                        imagen = url
+                    }
+                   
+                    libros[index] = {imagen,id, nombre, autor,ISBN, detalle, ejemplares}
+                 });
+            return libros;
+            } return [];
+            
+        }, noImagen);
+       
+        await navegador.close();
+        res.json({
+            resultados
+        });
+
+    } catch (error) {
+        res.status(500).send(`Algo salió mal: ${error}`);
+    }
+}
+
+const detalles = async(req = request, res = response)=>{
+    const { id } = req.params;
+    const noImagen = req.protocol + '://' + req.get('host') + '/images/no-imagen.jpg';
+    try {
+        const navegador = await chromium.launch({ chromiumSandbox: false });
+        const page = await navegador.newPage();
+        await page.goto(`https://opac.univalle.edu.co/cgi-olib/?oid=${id}`);
+        await page.waitForLoadState();
+        await page.waitForTimeout(20000);
+        const imagen = await page.evaluate((url)=>{
+            if(document.querySelector('#details_bkjacket>a') != null) return document.querySelector('#details_bkjacket>a').href.replace('-M.', '-L.')
+            return url;
+        }, noImagen);
+        const resumen = await page.evaluate(()=>{
+            if(document.querySelector('div#details_abstract_tab>div.details_tab_other') != null) {
+                return document.querySelector('div#details_abstract_tab>div.details_tab_other').innerText;
+            }
+            return 'Resumen no disponible';
+        });
+        const detallesDisponibilidad = await page.evaluate(()=>{
+            if(document.querySelector('.details_tab_copy.tabcont_vscroll_full>table>tbody') != null){
+                const contenido = document.querySelector('.details_tab_copy.tabcont_vscroll_full>table>tbody').querySelectorAll('tr');
+
+                const disponibilidad = new Array(contenido.length - 1);
+                for(let i = 0; i < disponibilidad.length; i++ ){
+                    const contenidoEjemplar = contenido[i + 1].querySelectorAll('td');
+                    const detalle = new Array(contenidoEjemplar.length);
+                        contenidoEjemplar.forEach((ubicacion, j)=>{
+                            detalle[j] = ubicacion.innerText;
+                        });
+                    const [codigo, localizacion, estante, signatura,coleccion, estado, categoria] = detalle;
+                    disponibilidad[i] = {codigo,localizacion, estante, signatura,coleccion,estado, categoria}   
+                  
+                }
+            return disponibilidad
+        
+            } return [];
+            
+        });
+
+        await navegador.close();
+        res.json({
+            imagen,
+            resumen,
+            detallesDisponibilidad
+        });
+
+    } catch (error) {
+        res.status(500).send(`Algo salió mal: ${error}`);
+    }
+}
+
 module.exports = {
     getInformacionPrimariaOPAC,
     actualizarLibro,
-    actualizarTodo
+    actualizarTodo,
+    detalles,
+    buscador
 }
--- routers/sistema_opac.router.js
+++ routers/sistema_opac.router.js
@@ -1,11 +1,12 @@
 const {Router } = require('express');
-const { getInformacionPrimariaOPAC, actualizarLibro, actualizarTodo } = require('../controllers/sistema_opac.controller');
+const { getInformacionPrimariaOPAC, actualizarLibro, actualizarTodo, buscador, detalles } = require('../controllers/sistema_opac.controller');
 
 const router = Router();
 
 router.get('/', getInformacionPrimariaOPAC);
 router.get('/actualizar/:index', actualizarLibro);
 router.get('/actualizar',actualizarTodo)
-
+router.get('/buscar', buscador);
+router.get('/buscar/:id', detalles);
 
 module.exports = router;
