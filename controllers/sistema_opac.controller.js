626706df0b23de2f6dbb994f65497940ea800b96
code3743 <jota.lopez@correounivalle.edu.co>
2022-08-17 20:18:32 -0500

    Refactorizar

--- controllers/sistema_opac.controller.js
+++ controllers/sistema_opac.controller.js
@@ -12,14 +12,12 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
     await page.evaluate(()=>{
         document.querySelector('input[value=" Ingresar "]').click();
     })
-    
     await page.waitForTimeout(5000)
     const evaluarUsuario = await page.evaluate(()=>{
         if(document.querySelector('#login1') == null){
             return true;
         }else return false;
     });
-   
     if(!evaluarUsuario) {
         return  res.status(400).json({
             error: 'El usuario no existe',
@@ -59,7 +57,6 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
           document.querySelector('#tab_Title3').click();
           return [];
       });
-  
       const historialPrestamos = await page.evaluate(()=>{
           if(document.querySelector('#tabcontent_Title3>#user_tab_hist>.details_tab_copy>table>tbody') != null){
               const  libros = document.querySelector('.details_tab_copy>table>tbody').querySelectorAll('tr');
@@ -80,7 +77,6 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
          return [];
       });
       await navegador.close();
-
       res.json({
             nombre,
             apellido,
@@ -89,8 +85,6 @@ const getInformacionPrimariaOPAC = async (req = request, res = response)=>{
             librosPrestados,
             historialPrestamos 
          });
-    
-  
     } catch (error) {
             res.status(500).send(`Algo salió mal: ${error}`);
      } 
@@ -109,7 +103,6 @@ const actualizarLibro = async (req = request, res = response)=>{
             document.querySelector('input[value=" Ingresar "]').click();
         })
         await page.waitForTimeout(5000);
-    
         await page.evaluate((index)=>{
           const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr');
           libros[index].querySelectorAll('td')[5].querySelector('span>img').click();
@@ -142,11 +135,9 @@ const actualizarTodo = async (req = request, res = response)=>{
             document.querySelector('input[value=" Ingresar "]').click();
         })
         await page.waitForTimeout(5000);
-    
         await page.evaluate(()=>{
             document.querySelector('img[title="Renovar todos los artículos"]').click();
           });
-
         await page.waitForTimeout(2000);
         const actualizarLosLibros = await page.evaluate(()=>{
             const libros = document.querySelector('.details_tab_copy').querySelectorAll('table>tbody>tr');
@@ -165,7 +156,6 @@ const actualizarTodo = async (req = request, res = response)=>{
         res.status(500).send(`Algo salió mal: ${error}`);
      } 
 };
-
 
 const buscador = async(req = request, res = response)=>{
     const { parametro } = req.query;
@@ -186,12 +176,10 @@ const buscador = async(req = request, res = response)=>{
                     const id = libro.querySelector('.resultsbright>a').id.replace('hitlabel','');
                     const nombre = libro.querySelector('.resultsbright').innerText;
                     const autor = libro.querySelector('.extras').innerText.split('\n')[1];
-
                     let ISBN = '';
                     let detalle = '';
                     let ejemplares = '';
                     let imagen = '';
-                    
                     if(libro.querySelector('.extras').innerText.includes('ISBN: ')){
                         ISBN  = libro.querySelector('.extras').innerText.split('\n')[2].replaceAll('ISBN: ','');
                         detalle  = libro.querySelector('.tihitlist_l3').innerText.split('\n')[0].replace(' ','');
@@ -202,20 +190,17 @@ const buscador = async(req = request, res = response)=>{
                         detalle = libro.querySelector('.tihitlist_l3').innerText.split('\n')[0].replace(' ','');
                         ejemplares = libro.querySelector('.tihitlist_l3').innerText.split('\n')[1];
                         imagen = url
-                    }
-                   
+                    }         
                     libros[index] = {imagen,id, nombre, autor,ISBN, detalle, ejemplares}
                  });
             return libros;
             } return [];
             
         }, noImagen);
-       
         await navegador.close();
         res.json({
             resultados
         });
-
     } catch (error) {
         res.status(500).send(`Algo salió mal: ${error}`);
     }
@@ -243,7 +228,6 @@ const detalles = async(req = request, res = response)=>{
         const detallesDisponibilidad = await page.evaluate(()=>{
             if(document.querySelector('.details_tab_copy.tabcont_vscroll_full>table>tbody') != null){
                 const contenido = document.querySelector('.details_tab_copy.tabcont_vscroll_full>table>tbody').querySelectorAll('tr');
-
                 const disponibilidad = new Array(contenido.length - 1);
                 for(let i = 0; i < disponibilidad.length; i++ ){
                     const contenidoEjemplar = contenido[i + 1].querySelectorAll('td');
@@ -252,22 +236,17 @@ const detalles = async(req = request, res = response)=>{
                             detalle[j] = ubicacion.innerText;
                         });
                     const [codigo, localizacion, estante, signatura,coleccion, estado, categoria] = detalle;
-                    disponibilidad[i] = {codigo,localizacion, estante, signatura,coleccion,estado, categoria}   
-                  
+                    disponibilidad[i] = {codigo,localizacion, estante, signatura,coleccion,estado, categoria}        
                 }
             return disponibilidad
-        
             } return [];
-            
         });
-
         await navegador.close();
         res.json({
             imagen,
             resumen,
             detallesDisponibilidad
         });
-
     } catch (error) {
         res.status(500).send(`Algo salió mal: ${error}`);
     }
